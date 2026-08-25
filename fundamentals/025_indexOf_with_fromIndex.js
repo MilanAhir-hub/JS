1a893760dafad7135569c8e8875fd2f1cf3a910f
MilanAhir-hub <milangagiya10@gmail.com>
2026-08-25 11:23:26 +0530

    index of with from index

--- fundamentals/025_indexOf_with_fromIndex.js
+++ fundamentals/025_indexOf_with_fromIndex.js
@@ -15,11 +15,23 @@
  * - Return index or -1
  */
 
-function findNextIndex(arr, target, startFrom) {
-  return arr.indexOf(target, startFrom);
+// function findNextIndex(arr, target, startFrom) {
+//   return arr.indexOf(target, startFrom);
+// }
+
+function findNextIndex(arr, target, startFrom){
+
+  for(let i=startFrom; i<arr.length; i++){
+    if(arr[i] == target){
+      return i;
+    }
+  }
+
+  return -1;
+
 }
 
 // Test Cases
-console.log(findNextIndex(["a", "b", "c", "b", "a", "b"], "b", 4)); // Expected output: 3
+console.log(findNextIndex(["a", "b", "c", "b"], "b", 2)); // Expected output: 3
 
 module.exports = { findNextIndex };
